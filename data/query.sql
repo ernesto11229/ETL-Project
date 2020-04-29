06278f5a26e157dad10f3a942be8aaab3ee50f4d
ernesto11229 <ernesto11229@hotmail.com>
2020-04-28 20:16:39 -0700

    table query, and cause of deaths count query

---
+++ data/query.sql
@@ -0,0 +1,19 @@
+select state, 
+	count (*) as death_count,
+	count(case when cause = 'Gunfire' then 1 else null end) as gun_deaths,
+	count(case when cause = 'Vehicular assault' then 1 else null end) as vehicular_assault_deaths,
+	count(case when cause = 'Struck by train' then 1 else null end) as struck_train_deaths,
+	count(case when cause = 'Stabbed' then 1 else null end) as stabbed_deaths,
+	count(case when cause = 'Motorcycle accident' then 1 else null end) as motorcycl_accident_deaths,
+	count(case when cause = 'Heat exhaustion' then 1 else null end) as heat_exhaustion_deaths,
+	count(case when cause = 'Heart attack' then 1 else null end) as heart_attack_deaths,
+	count(case when cause = 'Duty related illness' then 1 else null end) as duty_related_illness_deaths,
+	count(case when cause = 'Drowned' then 1 else null end) as drowned_deaths,
+	count(case when cause = 'Automobile accident' then 1 else null end) as automobile_accident_deaths,
+	count(case when cause = 'Assault' then 1 else null end) as assult_deaths
+from police_deaths where canine = false group by state;
+
+
+
+
+
---
+++ data/tables.sql
@@ -0,0 +1,21 @@
+drop table gun_deaths;
+
+CREATE TABLE gun_deaths (
+ 	county VARCHAR NOT NULL,
+ 	state VARCHAR NOT NULL,
+ 	deaths INT,
+ 	population int,
+	crude_rate decimal
+
+);
+
+
+CREATE TABLE police_deaths (
+ 	cause VARCHAR NOT NULL,
+ 	date DATE,
+ 	canine boolean,
+ 	state varchar
+	
+);
+
+
