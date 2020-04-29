a85acd5b581a2b5cba82e526e0718214376ac761
ernesto11229 <ernesto11229@hotmail.com>
2020-04-28 21:41:25 -0700

    joined sub querys on state column, created columns of each cause of deaths, added a column for percent of police officers that die from guns in each state

--- data/query.sql
+++ data/query.sql
@@ -1,3 +1,4 @@
+with pd as (
 select state, 
 	count (*) as death_count,
 	count(case when cause = 'Gunfire' then 1 else null end) as gun_deaths,
@@ -11,13 +12,33 @@ select state,
 	count(case when cause = 'Drowned' then 1 else null end) as drowned_deaths,
 	count(case when cause = 'Automobile accident' then 1 else null end) as automobile_accident_deaths,
 	count(case when cause = 'Assault' then 1 else null end) as assult_deaths
-from police_deaths where canine = false group by state;
+from police_deaths where canine = false group by state
+)
+
+select 
+	pd.state,
+	pd.death_count,
+	pd.gun_deaths,
+	pd.vehicular_assault_deaths,
+	pd.struck_train_deaths,
+	pd.stabbed_deaths,
+	pd.motorcycl_accident_deaths,
+	pd.heat_exhaustion_deaths,
+	pd.heart_attack_deaths,
+	pd.duty_related_illness_deaths,
+	pd.drowned_deaths,
+	pd.automobile_accident_deaths,
+	pd.assult_deaths,
+	nopd.not_only_police_death_count,
+	nopd.not_only_police_death_count / pd.death_count as police_death_rate_percent
+from (select state, 
+	sum (deaths) as not_only_police_death_count 
+from gun_deaths group by state) as nopd
+
+join pd on pd.state = nopd.state
+order by pd.state;
 
 
-select state, 
-	sum (deaths) as not_only_police_death_count
-	
-from gun_deaths group by state;
 
 
 
